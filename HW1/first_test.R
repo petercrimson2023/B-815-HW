0f860e4cbb2e85b950ba456dae211c0fe7f60045
petercrimson2023 <crimsonpeter317@gmail.com>
2024-01-20 20:28:27 -0500

    adding grad_pi function

--- HW1/first_test.R
+++ HW1/first_test.R
@@ -1,5 +1,7 @@
 # k=2 case
 
+library(dplyr)
+
 u1 = 5
 sigma1 = 2
 
@@ -18,11 +20,11 @@ data_points = (uniform_random>pi_1) * rnorm(n, u1, sigma1) + (uniform_random<=pi
 
 u_init = c(1,1)
 sigma_init = c(2,2)
-pi_init = c(0.5,0.5)
+pi_init = c(1,3)
 
 
 
-dnorm(data_points,u_init[1],sigma_init[1])
+#dnorm(data_points,u_init[1],sigma_init[1])
 
 # loss function
 
@@ -30,6 +32,9 @@ loss_function = function(data_points,u,sigma,pi_value){
   n = length(data_points)
   k = length(u)
   loss = 0
+  
+  pi_value = pi_value^2/sum(pi_value^2)
+  
   density_list = list()
   for(i in 1:k){
     density_list[[i]] = dnorm(data_points,u[i],sigma[i])
@@ -40,5 +45,68 @@ loss_function = function(data_points,u,sigma,pi_value){
   
   return(-loss)
 }
+
+#loss_function(data_points,u_init,sigma_init,pi_init)
+
+
+
+
+
+# gradient function
+
+pi_grad = function(density_matrix, u, sigma, pi_value) {
+  
+  inverse_term = (density_matrix %*% pi_init^2/sum(pi_init^2)) %>% as.vector()
+  
+  inverse_term = 1 / inverse_term
+  
+  second_term_matrix =  matrix(0, nrow = k, ncol = k)
+  
+  for (i in 1:k)
+  {
+    for (j in 1:k)
+    {
+      if (i == j)
+      {
+        second_term_matrix[i, j] = 2 * pi_value[i] * sum(pi_value[-i]^2) / (sum(pi_value^2)^2)
+      }
+      else
+      {
+        second_term_matrix[i, j] = -2 * pi_value[i] / (sum(pi_value^2)^2)
+      }
+    }
+  }
+  
+  pi_grad_vec = ((density_matrix %*% second_term_matrix) * inverse_term)  %>% colSums()
+  
+  return(pi_grad_vec)
+  
+}
+
+grad_calculate = function(data_points,u,sigma,pi_value){
+  
+  density_list = list()
+  k = length(u)
+  for (i in 1:k)
+  {
+    density_list[[i]] = dnorm(data_points, u[i], sigma[i])
+  }
+  density_matrix = data.frame(density_list) %>% as.matrix()
+  
+  pi_grad_vec = pi_grad(density_matrix, u, sigma, pi_value)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
